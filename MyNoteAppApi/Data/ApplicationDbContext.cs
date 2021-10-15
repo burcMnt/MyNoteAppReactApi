756d20ee2cbc03b64b31c45a3562bd810ba08400
burcMnt <burcumantar0@gmail.com>
2021-10-15 13:03:27 +0300

    Add project files.

---
+++ MyNoteAppApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyNoteAppApi.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Note> Notes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Note>().HasData(
+                new Note() { Id = 1, Title = "Simple note1", Content = "Lorem ipsum sit amet,consectetur." },
+                new Note() { Id = 2, Title = "Simple note2", Content = "Set et luctus dolor, eget ullacorper urna." });
+        }
+    }
+}
---
+++ MyNoteAppApi/Migrations/20211015085415_initial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyNoteAppApi.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Notes",
+                columns: new[] { "Id", "Content", "CreateTime", "ModifiedTime", "Title" },
+                values: new object[] { 1, "Lorem ipsum sit amet,consectetur.", new DateTime(2021, 10, 15, 11, 54, 15, 221, DateTimeKind.Local).AddTicks(527), null, "Simple note1" });
+
+            migrationBuilder.InsertData(
+                table: "Notes",
+                columns: new[] { "Id", "Content", "CreateTime", "ModifiedTime", "Title" },
+                values: new object[] { 2, "Set et luctus dolor, eget ullacorper urna.", new DateTime(2021, 10, 15, 11, 54, 15, 222, DateTimeKind.Local).AddTicks(3823), null, "Simple note2" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
